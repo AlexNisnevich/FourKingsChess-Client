7e266dc83f84cacfb674939f28f75f9641ff76be
AlexNisnevich <alex.nisnevich@gmail.com>
2011-07-30 00:25:26 -0700

    Country selection

--- js/start.js
+++ js/start.js
@@ -1,9 +1,35 @@
 var game = new Game();
 game.setup();
 
-game.addPlayer('Athens', 'red');
-game.addPlayer('Mongols', 'green');
-game.addPlayer('PapalStates', 'yellow');
-game.addPlayer('Britain', 'blue');
+var countries = ['Athens', 'Britain', 'Mongols', 'PapalStates', 'Sparta'];
 
-game.startGame();
+$$('#setup select').each(function(dropdown) {
+	var blankOption = new Element('option', {
+        'value': '',
+        html: '(Choose a country)'
+    });
+	blankOption.inject(dropdown);
+	
+	countries.each(function (countryName) {
+		var newOption = new Element('option', {
+            'value': countryName,
+            html: countryName
+        });
+		newOption.inject(dropdown);
+	});
+});
+
+$('startButton').addEvent('click', function() {
+	if ($$('#setup select').every(function (dropdown) {
+		return (dropdown.getSelected()[0].value != '');
+	})) {
+			game.addPlayer($('selectRed').getSelected()[0].value, 'red');
+			game.addPlayer($('selectGreen').getSelected()[0].value, 'green');
+			game.addPlayer($('selectYellow').getSelected()[0].value, 'yellow');
+			game.addPlayer($('selectBlue').getSelected()[0].value, 'blue');
+			game.startGame();
+			
+			$('setup').dispose();
+			$('moves').show();
+	}
+});
